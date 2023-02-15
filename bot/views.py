540e7bb21329ce316308fd16184f0363e2765874
asd95 <asd95027@gmail.com>
2023-02-15 22:20:17 +0800

    完成大樂透函式

--- bot/views.py
+++ bot/views.py
@@ -9,6 +9,12 @@ from linebot import LineBotApi, WebhookHandler,WebhookParser
 from linebot.exceptions import InvalidSignatureError,LineBotApiError
 from linebot.models import MessageEvent,TextSendMessage,TextMessage,ImageSendMessage
 import random
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import json
+import time
+from datetime import datetime
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parse=WebhookParser(settings.LINE_CHANNEL_SECRET)
@@ -35,12 +41,20 @@ def callback(request):
                     message,image_url=None,None
                     text=event.message.text
                     print(text)  
-                    if '電影' in text:
-                        message='https://movies.yahoo.com.tw/'
-                    elif '台北捷運' in text:
-                        message='https://web.metro.taipei/pages/assets/images/routemap2020.png'
-                    elif '台中捷運' in text:
-                        image_url='https://assets.piliapp.com/s3pxy/mrt_taiwan/taichung/20201112_zh.png'                       
+                    if '大樂透'==text:
+                        message=get_biglotto()
+                    elif '中文' in text:
+                        message=get_cmusic()
+                    elif '日文' in text:
+                        message=get_jmusic()
+                    elif '西洋' in text:
+                        message=get_wmusic()
+                    elif '韓文' in text:
+                        message=get_kmusic()
+                    elif '台語' in text:
+                        message=get_tmusic()
+                    elif '粵語' in text:
+                        message=get_hmusic()              
                     elif '樂透' in text:
                         message = lotto()
                     elif '早安' in text:
@@ -68,3 +82,116 @@ def lotto():
     n = random.randint(1, 50)
 
     return (f'{result} 特別號:{n}')
+
+
+def get_biglotto():
+    url='https://www.taiwanlottery.com.tw/lotto/lotto649/history.aspx'
+    resp=requests.get(url)
+    soup=BeautifulSoup(resp.text,'lxml')
+    trs=soup.find('table',class_='table_org td_hm').find_all('tr')
+    numbers=[td.text.strip() for td in trs[4].find_all('td')[1:]]
+    numbers=' '.join(numbers[:-1])+' 特別號:'+numbers[-1]
+    data1=[td.text.strip() for td in trs[0].find_all('td')]
+    data2=[td.text.strip() for td in trs[1].find_all('td')]
+    data=list(zip(data1,data2))
+    title=':'.join(np.array(data).reshape(10))
+    result=f'{title}\n{numbers}'
+    return result
+
+def get_cmusic():
+    url="https://kma.kkbox.com/charts/api/v1/daily?category=297&lang=tc&limit=50&terr=tw&type=newrelease"
+    response=requests.get(url)
+    data=json.loads(response.text)
+    song_list = data["data"]["charts"]["newrelease"]
+    temp_str=''
+    for i, song in enumerate(song_list):
+        song_rank=song["rankings"]["this_period"]
+        last_rank=song["rankings"]["last_period"]
+        song_name=song["song_name"]
+        song_artist=song["artist_name"]
+        song_url=song["song_url"]    
+        temp_str += f'{i+1}. {song_artist}-{song_name} 今日排名:{song_rank}, 上期排名:{last_rank}, 連結:{song_url}\n'
+    return temp_str
+
+def get_jmusic(): 
+    url1="https://kma.kkbox.com/charts/api/v1/daily?category=308&lang=tc&limit=50&terr=tw&type=newrelease"
+    response1=requests.get(url1)
+    data1=json.loads(response1.text)
+    song_list1 = data1["data"]["charts"]["newrelease"]
+    temp_str1='' 
+    for i, song1 in enumerate(song_list1):
+        song_rank1=song1["rankings"]["this_period"]
+        last_rank1=song1["rankings"]["last_period"]
+        song_name1=song1["song_name"]
+        song_artist1=song1["artist_name"]
+        song_url1=song1["song_url"]    
+        temp_str1 += f'{i+1}. {song_artist1}-{song_name1} 今日排名:{song_rank1}, 上期排名:{last_rank1}, 連結:{song_url1}\n'
+    return temp_str1 
+        
+
+
+def get_wmusic():
+    url2="https://kma.kkbox.com/charts/api/v1/daily?category=390&lang=tc&limit=50&terr=tw&type=newrelease"
+    response2=requests.get(url2)
+    data2=json.loads(response2.text)
+    song_list2 = data2["data"]["charts"]["newrelease"]
+    temp_str2=''
+    for i, song2 in enumerate(song_list2):
+        song_rank2=song2["rankings"]["this_period"]
+        last_rank2=song2["rankings"]["last_period"]
+        song_name2=song2["song_name"]
+        song_artist2=song2["artist_name"]
+        song_url2=song2["song_url"]    
+        temp_str2 += f'{i+1}. {song_artist2}-{song_name2} 今日排名:{song_rank2}, 上期排名:{last_rank2}, 連結:{song_url2}\n'
+    return temp_str2 
+
+
+
+
+def get_kmusic(): 
+    url3="https://kma.kkbox.com/charts/api/v1/daily?category=314&lang=tc&limit=50&terr=tw&type=newrelease"
+    response3=requests.get(url3)
+    data3=json.loads(response3.text)
+    song_list3 = data3["data"]["charts"]["newrelease"]
+    temp_str3=''
+    for i, song3 in enumerate(song_list3):
+        song_rank3=song3["rankings"]["this_period"]
+        last_rank3=song3["rankings"]["last_period"]
+        song_name3=song3["song_name"]
+        song_artist3=song3["artist_name"]
+        song_url3=song3["song_url"]    
+        temp_str3 += f'{i+1}. {song_artist3}-{song_name3} 今日排名:{song_rank3}, 上期排名:{last_rank3}, 連結:{song_url3}\n'
+    return temp_str3
+
+
+
+def get_tmusic(): 
+    url4="https://kma.kkbox.com/charts/api/v1/daily?category=304&lang=tc&limit=50&terr=tw&type=newrelease"
+    response4=requests.get(url4)
+    data4=json.loads(response4.text)
+    song_list4 = data4["data"]["charts"]["newrelease"]
+    temp_str4=''  
+    for i, song4 in enumerate(song_list4):
+        song_rank4=song4["rankings"]["this_period"]
+        last_rank4=song4["rankings"]["last_period"]
+        song_name4=song4["song_name"]
+        song_artist4=song4["artist_name"]
+        song_url4=song4["song_url"]    
+        temp_str4 += f'{i+1}. {song_artist4}-{song_name4} 今日排名:{song_rank4}, 上期排名:{last_rank4}, 連結:{song_url4}\n'
+    return temp_str4 
+
+
+def get_hmusic(): 
+    url5="https://kma.kkbox.com/charts/api/v1/daily?category=320&lang=tc&limit=50&terr=tw&type=newrelease"
+    response5=requests.get(url5)
+    data5=json.loads(response5.text)
+    song_list5 = data5["data"]["charts"]["newrelease"]
+    temp_str5=''   
+    for i, song5 in enumerate(song_list5):
+        song_rank5=song5["rankings"]["this_period"]
+        last_rank5=song5["rankings"]["last_period"]
+        song_name5=song5["song_name"]
+        song_artist5=song5["artist_name"]
+        song_url5=song5["song_url"]    
+        temp_str5 += f'{i+1}. {song_artist5}-{song_name5} 今日排名:{song_rank5}, 上期排名:{last_rank5}, 連結:{song_url5}\n'
+    return temp_str5
